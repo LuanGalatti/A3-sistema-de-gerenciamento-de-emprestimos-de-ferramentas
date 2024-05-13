3e5cc58d0586683339b4a14c9ead32f5f64b436a
pedrokrukz <ppok306@gmail.com>
2024-05-13 11:28:43 -0300

    Criei Ferramentas.java com nome/marca/custo e disponibilidade de empréstimo

--- src/main/java/model/Ferramenta.java
+++ src/main/java/model/Ferramenta.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+
 package model;
 
-/**
- *
- * @author 1072419087
- */
 public class Ferramenta {
-    
+    private final String nome;
+    private final String marca;
+    private final double custo;
+    private boolean disponivel;
+
+    public Ferramenta(String nome, double custo) {
+        this.nome = nome;
+        this.custo = custo;
+        this.marca = ""; // Inicializa a marca como uma string vazia
+        this.disponivel = true;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getCusto() {
+        return custo;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void emprestar() {
+        if (disponivel) {
+            disponivel = false;
+            System.out.println("Ferramenta " + nome + " emprestada");
+        } else {
+            System.out.println("Esta ferramenta está indisponível para empréstimo");
+        }
+    }
+
+    public void devolver() {
+        if (!disponivel) {
+            disponivel = true;
+            System.out.println("Ferramenta " + nome + " foi devolvida");
+        } else {
+            System.out.println("Ferramenta já está disponível no sistema");
+        }
+    }
 }
