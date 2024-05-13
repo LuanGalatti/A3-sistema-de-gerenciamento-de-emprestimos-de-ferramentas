b3e3ecca6da1a69041e7f9d7770facc3e907e0eb
1072419087 <1072419087@UNS07DT92259.animaedu.intranet>
2024-05-13 09:35:23 -0300

    Inicio do projeto + classes

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
-
-/**
- *
- * @author 1072419087
- */
+import dao.ClienteDAO;
 public class Cliente {
+    private String nomeCliente;
+    private String telefone;
+public Cliente(){
+    this("","");
+}
+    public Cliente(String nomeCliente, String telefone) {
+        this.nomeCliente = nomeCliente;
+        this.telefone = telefone;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+  
     
 }
