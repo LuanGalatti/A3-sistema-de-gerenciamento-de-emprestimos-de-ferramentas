851c00a20cd3eb8fa8c21b0017052e783220f892
Luan Galatti <60677273+LuanGalatti@users.noreply.github.com>
2024-05-20 10:18:16 -0300

    Feat: adicionei a tabela de ferramentas

--- banco.sql
+++ banco.sql
@@ -1,9 +1,19 @@
+#cria a database db_emprestimo
 create database if not exists db_emprestimo;
 use db_emprestimo;
-drop table if exists tb_Amigo;
+#cria a tabela "tb_Amigo" com os parametros idAmigo, nomeAmigo e telefoneAmigo
+#define a primary key da tb_Ferramenta como idAmigo
 CREATE TABLE IF NOT EXISTS `db_emprestimo`.`tb_Amigo` (
   `idAmigo` INT NOT NULL,
   `nomeAmigo` VARCHAR(45) NULL,
   `telefoneAmigo` VARCHAR(12) NULL,
-  PRIMARY KEY (`idAmigo`))
-ENGINE = InnoDB
+  PRIMARY KEY (`idAmigo`));
+#cria a tabela "tb_Ferramenta" com os parametros idFerramenta,nomeFerramenta, marcaFerramenta, curstoFerramenta
+#define a primary key da tb_Ferramenta como idFerramenta  
+CREATE TABLE IF NOT EXISTS `db_emprestimo`.`tb_Ferramenta` (
+  `idFerramenta` INT NOT NULL,
+  `nomeFerramenta` VARCHAR(45) NULL,
+   `marcaFerramenta` VARCHAR(45) NULL,
+    `custoFerramenta` VARCHAR(8) NULL,
+  PRIMARY KEY (`idFerramenta`));
+
